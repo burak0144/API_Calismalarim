80831b08ca555a34cc691fce149c4916b883e320
burak <karanfilkaranfil822@gmail.com>
2022-10-30 14:47:40 +0300

    30.10.22

--- src/test/java/get_request/Get05.java
+++ src/test/java/get_request/Get05.java
@@ -1,4 +1,19 @@
 package get_request;
 
+import org.junit.Test;
+
 public class Get05 {
+    @Test
+    public void get05() {
+        /*
+        Given
+            https://restful-booker.herokuapp.com/booking
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+      And
+         Among the data there should be someone whose firstname is "Johhny" and lastname is "Dear"
+     */
+    }
 }
