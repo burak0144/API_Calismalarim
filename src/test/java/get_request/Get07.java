22a10d49710a27f028ebf713ee5c2fce9b4ef57e
burak <karanfilkaranfil822@gmail.com>
2022-10-31 14:18:35 +0300

    30.10.22

--- src/test/java/get_request/Get07.java
+++ src/test/java/get_request/Get07.java
@@ -1,8 +1,19 @@
 package get_request;
 
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
-public class Get07 {
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+public class Get07 extends JsonplaceholderBaseUrl {
     @Test
     public void get07(){
 
@@ -21,5 +32,26 @@ public class Get07 {
             Assert that "delectus aut autem" is one of the titles whose id is less than 5 ==> id si 5 den kucuk olan datalarin birinin
             basliginin "delectus aut autem" icerdigini dogrulayin
      */
+        spec.pathParam("first","todos");
+        Response response=given().spec(spec).when().get("/{first}");
+        response.then().assertThat().statusCode(200);
+        ///assertEquals(200,response.statusCode());
+        JsonPath json=response.jsonPath();
+        SoftAssert softAssert=new SoftAssert();
+
+       // 2)Print all ids greater than 190 on the console ==> id si 190 dan buyuk olanlari konsola yazdirin
+        //body icinde islem yapacagimizda jsonPath ve Grovy Language kullanilir
+        List<Integer> ids =json.getList("findAll{it.id>190}.id");
+        System.out.println("Id'si 190 dan Buyuk Olanlar = " + ids);
+        //assertEquals("Id 190 dan buyuk olan eslesmedi",ids.size(),equalTo(10)); <10> actual bu sekilde oldugu icin
+                //asagidaki gibi yapmamiz lazim
+        assertEquals("Id 190 dan buyuk olan eslesmedi",10,ids.size());
+
+        // 3)Print all userIds whose ids are less than 5 on the console ==> id si 5 den kucuk olan tum userid lerini konsolunu yazdirin
+        List<Integer> userIds=json.getList("findAll{it.id<5}.userId");
+        System.out.println("Id 5'ten kucuk olan userIds ="+userIds);
+
+
+
     }
 }
