bb8b254d075605c14270c3801d698eb7f42fc6c2
burak <karanfilkaranfil822@gmail.com>
2022-10-31 14:30:19 +0300

    30.10.22

--- src/test/java/get_request/Get07.java
+++ src/test/java/get_request/Get07.java
@@ -12,6 +12,7 @@ import java.util.List;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Get07 extends JsonplaceholderBaseUrl {
     @Test
@@ -40,6 +41,7 @@ public class Get07 extends JsonplaceholderBaseUrl {
         SoftAssert softAssert=new SoftAssert();
 
        // 2)Print all ids greater than 190 on the console ==> id si 190 dan buyuk olanlari konsola yazdirin
+        // Assert that there are 10 ids greater than 190 == > 10 tane id nin 190 dan buyuk oldugunu dogrulayin
         //body icinde islem yapacagimizda jsonPath ve Grovy Language kullanilir
         List<Integer> ids =json.getList("findAll{it.id>190}.id");
         System.out.println("Id'si 190 dan Buyuk Olanlar = " + ids);
@@ -48,10 +50,18 @@ public class Get07 extends JsonplaceholderBaseUrl {
         assertEquals("Id 190 dan buyuk olan eslesmedi",10,ids.size());
 
         // 3)Print all userIds whose ids are less than 5 on the console ==> id si 5 den kucuk olan tum userid lerini konsolunu yazdirin
+        //Assert that the number of userIds whose ids are less than 5 is 4 ==> id si 5 den kucuk olan 4 tane userId oldugunu dogrulayin
         List<Integer> userIds=json.getList("findAll{it.id<5}.userId");
         System.out.println("Id 5'ten kucuk olan userIds ="+userIds);
+        assertEquals("Id'si 5 den kucuk olan User Id ler 4 tane Degil",4,userIds.size());
 
-
+       //4)Print all titles whose ids are less than 5 ==> id si 5 den kucuk olan tum basliklari yazdirin
+        List<String> titles=json.getList("findAll{it.id<5}.title");
+        System.out.println("Id 5'ten kucuk olan titles ="+titles);
+        //Assert that "delectus aut autem" is one of the titles whose id is less than 5 ==> id si 5 den kucuk olan datalarin birinin
+        //            basliginin "delectus aut autem" icerdigini dogrulayin
+        assertTrue("titles arasinda delectus aut autem bulunmuyor",titles.contains("delectus aut autem"));
+        titles.stream().anyMatch(t->t.equals("delectus aut autem"));
 
     }
 }
