ac9079a62f3509da4f45b1adb2af041e91da6248
burak <karanfilkaranfil822@gmail.com>
2022-10-31 13:48:32 +0300

    30.10.22

--- src/test/java/get_request/Get06.java
+++ src/test/java/get_request/Get06.java
@@ -1,8 +1,16 @@
 package get_request;
 
+import base_url.RestfulBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
-public class Get06 {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Get06 extends RestfulBaseUrl {
     @Test
     public void get06(){
         /*
@@ -28,6 +36,36 @@ public class Get06 {
     "additionalneeds": "None"
 }
      */
+
+        spec.pathParams("first","booking","second","2325");
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+
+        //body icerisindekileri sadece dogrulama yapmak icin asagidaki gibi yapabiliriz
+        response.then().statusCode(200).
+                contentType(ContentType.JSON).
+                body("firstname",equalTo("Jim"),
+                        "lastname",equalTo("Brown"),
+                        "totalprice",equalTo(111),
+                        "depositpaid",equalTo(true),
+                        "bookingdates.checkin",equalTo("2018-01-01"),
+                        "bookingdates.checkout",equalTo("2019-01-01"),
+                        "additionalneeds",equalTo("Breakfast"));
+
+        //JsonPath ile body icerisinde istedigimiz islemi yapabiliriz,Bunun icin JsonPath ile body icine girilir
+       JsonPath json=response.jsonPath();
+        SoftAssert softAssert=new SoftAssert();
+
+        //yukardaki gibi yapamayiz cunku bize boolean donderiyor
+
+        softAssert.assertEquals("Jim",json.getString("firstname"));
+        softAssert.assertEquals("Brown",json.getString("lastname"));
+        softAssert.assertEquals(111,json.getInt("totalprice"));
+        softAssert.assertEquals("2018-01-01",json.getString("bookingdates.checkin"));
+        softAssert.assertEquals("2019-01-01",json.getString("bookingdates.checkout"));
+        softAssert.assertEquals("Breakfast",json.getString("additionalneeds"));
+
+        softAssert.assertAll();
+
 
     }
 }
