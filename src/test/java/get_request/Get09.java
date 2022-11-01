03d404049357d522e72c8e576816e9d47ee389ca
burak <karanfilkaranfil822@gmail.com>
2022-11-01 17:31:45 +0300

    30.10.22

--- src/test/java/get_request/Get09.java
+++ src/test/java/get_request/Get09.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class Get09 extends RestfulBaseUrl {
     @Test
@@ -58,8 +59,10 @@ public class Get09 extends RestfulBaseUrl {
         softAssert.assertEquals(json.getString("bookingdates.checkout"),"2014-10-23");
         softAssert.assertEquals(json.getString("additionalneeds"),"Breakfast");
 
+        softAssert.assertAll();
+
         //3.yol map ile
-        Map<String,Object> expectedDataBookingdates=new HashMap<>();  //once inner map yapildi
+        Map<String,String> expectedDataBookingdates=new HashMap<>();  //once inner map yapildi
         expectedDataBookingdates.put("checkin","2013-02-23");
         expectedDataBookingdates.put("checkout","2014-10-23");
 
@@ -74,27 +77,29 @@ public class Get09 extends RestfulBaseUrl {
 
         Map<String,Object> actualData=response.as(HashMap.class); //response da bulunan json lar javaya cevrilerek actualdatalar olsturuldu
         //karsilastirma yapalim simdi
-        softAssert.assertEquals(expectedDataAll.get("firstname"),actualData.get("firstname"));
-        softAssert.assertEquals(expectedDataAll.get("lastname"),actualData.get("lastname"));
-        softAssert.assertEquals(expectedDataAll.get("totalprice"),actualData.get("totalprice"));
-        softAssert.assertEquals(expectedDataAll.get("checkin"),((Map)actualData.get("bookingdates")).get("checkin"));
-        softAssert.assertEquals(expectedDataAll.get("checkout"),((Map)actualData.get("bookingdates")).get("checkout"));
+        assertEquals(expectedDataAll.get("firstname"),actualData.get("firstname"));
+        assertEquals(expectedDataAll.get("lastname"),actualData.get("lastname"));
+        assertEquals(expectedDataAll.get("totalprice"),actualData.get("totalprice"));
+        assertEquals(expectedDataBookingdates.get("checkin"), ((Map)(actualData.get("bookingdates"))).get("checkin"));
+        assertEquals(expectedDataBookingdates.get("checkout"),((Map)actualData.get("bookingdates")).get("checkout"));
         //yukarda map icinde map oldugundan dolayi get methoduyla map icindeki checkin ve checkout cagrildi
-        softAssert.assertEquals(expectedDataAll.get("additionalneeds"),actualData.get("additionalneeds"));
+        assertEquals(expectedDataAll.get("additionalneeds"),actualData.get("additionalneeds"));
 
         //4.yol map ile dinamik
         RestfulTestData obj = new RestfulTestData();
         Map<String, Object> expectedDataBookingdates2=obj.expectedDataBookingdatesMethod("2013-02-23","2014-10-23");//inner map
-        Map<String,Object>expectedDataAll2=obj.expectedDataAllMethod("Sally","Brown",111,expectedDataBookingdates2,"additionalneeds");//outer map
+        Map<String,Object>expectedDataAll2=obj.expectedDataAllMethod("Sally","Brown",111,expectedDataBookingdates2,"Breakfast");//outer map
         //dinamik yontemde testdatalarini diger classtan alarak yaptik,yukardaki satirdaki expectedDataBookingdates2'yi inner mapte bi ust satirda olusturdugumuzu yazdik
         Map<String,Object> actualData2=response.as(HashMap.class);
         //karsilastirma yapalim
-        softAssert.assertEquals(expectedDataAll2.get("firstname"),actualData2.get("firstname"));
-        softAssert.assertEquals(expectedDataAll2.get("lastname"),actualData2.get("lastname"));
-        softAssert.assertEquals(expectedDataAll2.get("totalprice"),actualData2.get("totalprice"));
-        softAssert.assertEquals(expectedDataAll2.get("checkin"),((Map)actualData2.get("bookingdates")).get("checkin"));
-        softAssert.assertEquals(expectedDataAll2.get("checkout"),((Map)actualData2.get("bookingdates")).get("checkout"));
-        softAssert.assertEquals(expectedDataAll2.get("additionalneeds"),actualData2.get("additionalneeds"));
+        assertEquals(expectedDataAll2.get("firstname"),actualData2.get("firstname"));
+        assertEquals(expectedDataAll2.get("lastname"),actualData2.get("lastname"));
+        assertEquals(expectedDataAll2.get("totalprice"),actualData2.get("totalprice"));
+        assertEquals(expectedDataBookingdates2.get("checkin"),((Map)actualData2.get("bookingdates")).get("checkin"));
+        assertEquals(expectedDataBookingdates2.get("checkout"),((Map)actualData2.get("bookingdates")).get("checkout"));
+        assertEquals(expectedDataAll2.get("additionalneeds"),actualData2.get("additionalneeds"));
+
+
 
 
     }
