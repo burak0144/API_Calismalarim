994f9c103c071f07177d8607b459aaaf7bef1f16
burak <karanfilkaranfil822@gmail.com>
2022-11-04 18:57:14 +0300

    3.11.22

--- src/test/java/post_request/Post01.java
+++ src/test/java/post_request/Post01.java
@@ -1,6 +1,18 @@
 package post_request;
 
-public class Post01 {
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.JsonplaceholderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post01 extends JsonplaceholderBaseUrl {
  /*
          Given
            1)  https://jsonplaceholder.typicode.com/todos
@@ -21,5 +33,21 @@ public class Post01 {
                                     "id": 201
                                     }
    */
+
+    @Test
+    public void post01() {
+        spec.pathParam("first","todos");
+        JsonplaceholderTestData obj= new JsonplaceholderTestData();
+        Map<String,Object> expectedData = obj.expectedDataMethod(55,"Tidy your room",false);
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+
+        Map<String,Object>actualData=response.as(HashMap.class);
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+
+        System.out.println("expectedData = " + expectedData);
+        System.out.println("actualData = " + actualData);
+    }
 }
 
