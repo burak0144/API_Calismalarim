004297de916fe58d5225f5553b815009dceeb324
burak <karanfilkaranfil822@gmail.com>
2022-11-04 14:45:44 +0300

    3.11.22

---
+++ src/test/java/post_request/Post01.java
@@ -0,0 +1,25 @@
+package post_request;
+
+public class Post01 {
+ /*
+         Given
+           1)  https://jsonplaceholder.typicode.com/todos
+           2)  {
+                 "userId": 55,
+                 "title": "Tidy your room",
+                 "completed": false
+                }
+        When
+         I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+   */
+}
+
