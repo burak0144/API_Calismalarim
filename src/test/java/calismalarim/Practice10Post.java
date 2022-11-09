b9ab4491ca95f581b51adfe7308cff1d04107cb0
burak <karanfilkaranfil822@gmail.com>
2022-11-09 23:24:48 +0300

    8.11.22

--- src/test/java/calismalarim/Practice10Post.java
+++ src/test/java/calismalarim/Practice10Post.java
@@ -1,11 +1,37 @@
 package calismalarim;
 
 import base_url.GMIBankBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import pojos.GMICountryDataPostPojo;
+import utilities.Authentication;
+
+import static io.restassured.RestAssured.given;
+import static utilities.Authentication.generateToken;
+
+
 public class Practice10Post extends GMIBankBaseUrl {
+    //yeni bir ulke ekleyin
     @Test
     public void test10() {
-        spec.pathParams("first","tp-customers","second",114351);
+        spec.pathParam("first", "tp-countries");
+
+        GMICountryDataPostPojo countryPost = new GMICountryDataPostPojo("MuzCumhuriyeti");
+
+
+        Response response = given().contentType(ContentType.JSON)
+                .headers("Authorization", "Bearer " + generateToken())
+                .spec(spec).when().body(countryPost)
+                .post("/{first}");
+
+        GMICountryDataPostPojo actualData=response.as(GMICountryDataPostPojo.class);
+
+        Assert.assertEquals(countryPost.getName(), actualData.getName());
+
+
     }
 }
---
+++ src/test/java/calismalarim/Practice11.java
@@ -0,0 +1,13 @@
+package calismalarim;
+
+public class Practice11 {
+    /*
+http://www.gmibank.com/api/tp-customers end point'ine
+request gönderin
+ 1) Tüm Customer bilgilerini ekrana yazdırın.
+ 2) Tüm Customer SSN lerini ekrana yazdırın.
+ 3) Tüm Customer SSN lerini text dosyası olarak kaydedin
+ 4) Olusturduğunuz text dosyasından  SSNleri okuyarak
+    "531-95-8437", "049-43-2360", "123-34-3434" SSNlerinin olduğunu doğrulayın
+ */
+}
