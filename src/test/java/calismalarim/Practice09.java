061ad1af2b98495f3be1cbfefb256e4f8651ed6d
burak <karanfilkaranfil822@gmail.com>
2022-11-09 22:02:53 +0300

    8.11.22

--- src/test/java/calismalarim/Practice09.java
+++ src/test/java/calismalarim/Practice09.java
@@ -1,6 +1,21 @@
 package calismalarim;
 
-public class Practice09 {
+import base_url.GMIBankBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.GMICountryDataPojo;
+import pojos.GMICustomerPojo;
+import pojos.GMIUsertDataPojo;
+import utilities.Authentication;
+import utils.ObjectMapperUtils;
+
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.responseSpecification;
+import static org.junit.Assert.assertEquals;
+
+public class Practice09 extends GMIBankBaseUrl {
     /*
     {
         “id”: 114351,
@@ -25,7 +40,44 @@ public class Practice09 {
         “user”: null,
         “accounts”: null
     },
-    {
+
+     */
+
+    @Test
+    public void test09() {
+        //Pojo ile cozum
+        spec.pathParams("first","tp-customers","second",114351);
+
+        GMICountryDataPojo countryDataPojo=new GMICountryDataPojo(3,"USA",null);
+        GMICustomerPojo expectedData=new GMICustomerPojo("Della","Heaney","Russell Homenick V","ricardo.larkin@yahoo.com","123-456-7893","213-456-7893","53081","75164 McClure Stream","Gislasonburgh","823-25-7239","2021-12-05T23:00:00Z",true,countryDataPojo,"New York43",null,null);
+
+        Response response=given().spec(spec).headers("Authorization","Bearer " + Authentication.generateToken()).when().get("/{first}/{second}");
+
+       GMICustomerPojo actualData=response.as(GMICustomerPojo.class);
+
+       assertEquals(expectedData.getFirstName(), actualData.getFirstName());
+       assertEquals(expectedData.getLastName(), actualData.getLastName());
+       assertEquals(expectedData.getMiddleInitial(), actualData.getMiddleInitial());
+       assertEquals(expectedData.getEmail(), actualData.getEmail());
+       assertEquals(expectedData.getMobilePhoneNumber(), actualData.getMobilePhoneNumber());
+       assertEquals(expectedData.getPhoneNumber(), actualData.getPhoneNumber());
+       assertEquals(expectedData.getZipCode(), actualData.getZipCode());
+       assertEquals(expectedData.getAddress(), actualData.getAddress());
+       assertEquals(expectedData.getCity(), actualData.getCity());
+       assertEquals(expectedData.getSsn(), actualData.getSsn());
+       assertEquals(expectedData.getCreateDate(), actualData.getCreateDate());
+       assertEquals(expectedData.getZelleEnrolled(), actualData.getZelleEnrolled());
+
+       assertEquals(countryDataPojo.getId(), actualData.getCountry().getId());
+       assertEquals(countryDataPojo.getName(), actualData.getCountry().getName());
+       assertEquals(countryDataPojo.getStates(), actualData.getCountry().getStates());
+
+       assertEquals(expectedData.getState(), actualData.getState());
+       assertEquals(expectedData.getUser(), actualData.getUser());
+
+    }
+/*
+{
         “id”: 110452,
         “firstName”: “Jasmine”,
         “lastName”: “Stehr”,
@@ -58,5 +110,44 @@ public class Practice09 {
         },
         “accounts”: null
     }
-     */
+ */
+    //ObjectMapper ile cozum
+    @Test
+    public void test10() {
+        spec.pathParams("first","tp-customers","second",110452);
+        GMICountryDataPojo countryDataPojo=new GMICountryDataPojo(3,"USA",null);
+        GMIUsertDataPojo userDataPojo=new GMIUsertDataPojo(110016,"leopoldo.reinger","Jasmine","Stehr","marni.zboncak@yahoo.com",true,"en",null,null);
+        GMICustomerPojo expectedData=new GMICustomerPojo("Jasmine","Stehr","V","marni.zboncak@yahoo.com","463-609-2097","1-112-497-0270","16525","14387 Al Ridge5343 Bert Burgs","Waltermouth","761-59-2911","2021-11-28T21:00:00Z",false,countryDataPojo,"California",null, Arrays.asList());
+
+        Response response=given().spec(spec).headers("Authorization","Bearer " + Authentication.generateToken()).when().get("/{first}/{second}");
+        GMICustomerPojo actualData= ObjectMapperUtils.convertJsonToJava(response.asString(),GMICustomerPojo.class);
+
+        assertEquals(expectedData.getFirstName(),actualData.getFirstName());
+        assertEquals(expectedData.getLastName(),actualData.getLastName());
+        assertEquals(expectedData.getMiddleInitial(),actualData.getMiddleInitial());
+        assertEquals(expectedData.getEmail(),actualData.getEmail());
+        assertEquals(expectedData.getMobilePhoneNumber(),actualData.getMobilePhoneNumber());
+        assertEquals(expectedData.getCity(),actualData.getCity());
+        assertEquals(expectedData.getSsn(),actualData.getSsn());
+        assertEquals(expectedData.getCreateDate(), actualData.getCreateDate());
+        assertEquals(expectedData.getZelleEnrolled(), actualData.getZelleEnrolled());
+
+        assertEquals(countryDataPojo.getId(), actualData.getCountry().getId());
+        assertEquals(countryDataPojo.getName(), actualData.getCountry().getName());
+        assertEquals(countryDataPojo.getStates(), actualData.getCountry().getStates());
+
+        assertEquals(expectedData.getState(), actualData.getState());
+
+        assertEquals(userDataPojo.getId(), actualData.getUser().getId());
+        assertEquals(userDataPojo.getLogin(), actualData.getUser().getLogin());
+        assertEquals(userDataPojo.getFirstName(), actualData.getUser().getFirstName());
+        assertEquals(userDataPojo.getLastName(), actualData.getUser().getLastName());
+        assertEquals(userDataPojo.getEmail(), actualData.getUser().getEmail());
+        assertEquals(userDataPojo.getActivated(), actualData.getUser().getActivated());
+        assertEquals(userDataPojo.getLangKey(), actualData.getUser().getLangKey());
+        assertEquals(userDataPojo.getImageUrl(), actualData.getUser().getImageUrl());
+        assertEquals(userDataPojo.getResetDate(), actualData.getUser().getResetDate());
+
+        assertEquals(expectedData.getAccounts(), actualData.getAccounts());
+    }
 }
--- src/test/java/pojos/GMICustomerPojo.java
+++ src/test/java/pojos/GMICustomerPojo.java
@@ -1,39 +1,196 @@
 package pojos;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GMICustomerPojo {
-/*
-{
-    “id”: 110452,
-    “firstName”: “Jasmine”,
-    “lastName”: “Stehr”,
-    “middleInitial”: “V”,
-    “email”: “marni.zboncak@yahoo.com”,
-    “mobilePhoneNumber”: “463-609-2097”,
-    “phoneNumber”: “1-112-497-0270”,
-    “zipCode”: “16525”,
-    “address”: “14387 Al Ridge5343 Bert Burgs”,
-    “city”: “Waltermouth”,
-    “ssn”: “761-59-2911”,
-    “createDate”: “2021-11-28T21:00:00Z”,
-    “zelleEnrolled”: false,
-    “country”: {
-        “id”: 3,
-        “name”: “USA”,
-        “states”: null
-    },
-    “state”: “California”,
-    “user”: {
-        “id”: 110016,
-        “login”: “leopoldo.reinger”,
-        “firstName”: “Jasmine”,
-        “lastName”: “Stehr”,
-        “email”: “marni.zboncak@yahoo.com”,
-        “activated”: true,
-        “langKey”: “en”,
-        “imageUrl”: null,
-        “resetDate”: null
-    },
-    “accounts”: []
-}
- */
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private String email;
+    private String mobilePhoneNumber;
+    private String phoneNumber;
+    private String zipCode;
+    private String address;
+    private String city;
+    private String ssn;
+    private String createDate;
+    private Boolean zelleEnrolled;
+    private GMICountryDataPojo country;
+    private String state;
+    private GMIUsertDataPojo user;
+    private Object accounts;
+
+    public GMICustomerPojo() {
+    }
+
+    public GMICustomerPojo(String firstName, String lastName, String middleInitial, String email, String mobilePhoneNumber, String phoneNumber, String zipCode, String address, String city, String ssn, String createDate, Boolean zelleEnrolled, GMICountryDataPojo country, String state, GMIUsertDataPojo user, Object accounts) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleInitial = middleInitial;
+        this.email = email;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.phoneNumber = phoneNumber;
+        this.zipCode = zipCode;
+        this.address = address;
+        this.city = city;
+        this.ssn = ssn;
+        this.createDate = createDate;
+        this.zelleEnrolled = zelleEnrolled;
+        this.country = country;
+        this.state = state;
+        this.user = user;
+        this.accounts = accounts;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public void setMobilePhoneNumber(String mobilePhoneNumber) {
+        this.mobilePhoneNumber = mobilePhoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public Boolean getZelleEnrolled() {
+        return zelleEnrolled;
+    }
+
+    public void setZelleEnrolled(Boolean zelleEnrolled) {
+        this.zelleEnrolled = zelleEnrolled;
+    }
+
+    public GMICountryDataPojo getCountry() {
+        return country;
+    }
+
+    public void setCountry(GMICountryDataPojo country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public GMIUsertDataPojo getUser() {
+        return user;
+    }
+
+    public void setUser(GMIUsertDataPojo user) {
+        this.user = user;
+    }
+
+    public Object getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Object accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "GMICustomerPojo{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleInitial='" + middleInitial + '\'' +
+                ", email='" + email + '\'' +
+                ", mobilePhoneNumber='" + mobilePhoneNumber + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", createDate='" + createDate + '\'' +
+                ", zelleEnrolled=" + zelleEnrolled +
+                ", country=" + country +
+                ", state='" + state + '\'' +
+                ", user=" + user +
+                ", accounts=" + accounts +
+                '}';
+    }
 }
